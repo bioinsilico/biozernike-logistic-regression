d1f73a8e7045df6789dc418e27a799e09afc6123
bioinsilico <jsegurmor@gmail.com>
2023-09-05 15:21:45 -0700

    log each epoch

--- src/biozernike_logistic_reg.py
+++ src/biozernike_logistic_reg.py
@@ -12,7 +12,7 @@ from sklearn.metrics import auc
 
 if __name__ == '__main__':
     learning_rate = 1e-6
-    epochs = 2000
+    epochs = 10000
     batch_size = 2 ** 8
     l2_weight = 1
     evaluation_step = 1000
@@ -29,25 +29,21 @@ if __name__ == '__main__':
     test_dataloader = DataLoader(testing_set, batch_size=len(testing_set))
     x_test, y_test = next(iter(test_dataloader))
 
-    writer = SummaryWriter(
-        log_dir="../tensorboard/runs",
-        comment=" | learning-rate: %s batch-size: %s l2-weight: %s" % (
-            learning_rate,
-            batch_size,
-            l2_weight
-        )
-    )
+    writer = SummaryWriter()
 
     model = LogisticRegression(input_features=3922)
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     writer.add_graph(model, x_test[0])
+    writer.add_text("learning-rate", "%s" % learning_rate)
+    writer.add_text("batch-size", "%s" % batch_size)
+    writer.add_text("l2-weight", "%s" % l2_weight)
+    writer.add_text("model-name", "%s" % type(model).__name__)
 
-    batch_n = 0
     print("Starting training. Number of batches: %s" % (len(train_dataloader)))
+    loss = None
     for epoch in range(epochs):
-        loss = None
         for x_train, y_train in train_dataloader:
             optimizer.zero_grad()
             y_predicted = model(x_train)
@@ -55,47 +51,43 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
 
-            if batch_n % evaluation_step == 0:
-                y_evaluation = model(x_test)
-                roc_auc = roc_auc_score(y_test.detach().numpy(), y_evaluation.detach().numpy())
-                precision, recall, thresholds = precision_recall_curve(
-                    y_test.detach().numpy(),
-                    y_evaluation.detach().numpy()
-                )
-                pr_auc = auc(recall, precision)
-                print("epoch %s, batch number %s, loss %s, testing roc %s auc %s" % (
-                    epoch,
-                    batch_n,
-                    loss.item(),
-                    roc_auc,
-                    pr_auc
-                ))
-                writer.add_scalar("Loss/1000batches", loss, batch_n // 1000)
-                writer.add_scalar("ROC/1000batches", roc_auc, batch_n // 1000)
-                writer.add_scalar("AUC/1000batches", pr_auc, batch_n // 1000)
-                writer.add_histogram("Weights/1000batches", model.get_weights(), batch_n // 1000)
-                writer.add_histogram("Bias/1000batches", model.get_bias(), batch_n // 1000)
-                sorted_evaluation = torch.Tensor.tolist(
-                    torch.squeeze(torch.argsort(y_evaluation, dim=0, descending=True))
-                )
-                writer.add_text(
-                    "High Prediction/Last batch",
-                    "  \n".join([
-                        "%s : %s : %s" % (
-                            testing_set.get_classes(idx), y_test[idx, 0].item(), y_evaluation[idx, 0].item())
-                        for idx in sorted_evaluation[0:1000]
-                    ])
-                )
-                writer.add_text(
-                    "Low Prediction/Last batch",
-                    "  \n".join([
-                        "%s : %s : %s" % (
-                            testing_set.get_classes(idx), y_test[idx, 0].item(), y_evaluation[idx, 0].item())
-                        for idx in sorted_evaluation[len(sorted_evaluation) - 1000:len(sorted_evaluation)]
-                    ])
-                )
-                writer.flush()
-
-            batch_n += 1
+        y_evaluation = model(x_test)
+        roc_auc = roc_auc_score(y_test.detach().numpy(), y_evaluation.detach().numpy())
+        precision, recall, thresholds = precision_recall_curve(
+            y_test.detach().numpy(),
+            y_evaluation.detach().numpy()
+        )
+        pr_auc = auc(recall, precision)
+        print("epoch %s, loss %s, testing roc %s auc %s" % (
+            epoch,
+            loss.item(),
+            roc_auc,
+            pr_auc
+        ))
+        writer.add_scalar("Loss/epoch", loss, epoch)
+        writer.add_scalar("ROC/epoch", roc_auc, epoch)
+        writer.add_scalar("AUC/epoch", pr_auc, epoch)
+        writer.add_histogram("Weights/epoch", model.get_weights(), epoch)
+        writer.add_histogram("Bias/epoch", model.get_bias(), epoch)
+        sorted_evaluation = torch.Tensor.tolist(
+            torch.squeeze(torch.argsort(y_evaluation, dim=0, descending=True))
+        )
+        writer.add_text(
+            "High Prediction/Last batch",
+            "  \n".join([
+                "%s : %s : %s" % (
+                    testing_set.get_classes(idx), y_test[idx, 0].item(), y_evaluation[idx, 0].item())
+                for idx in sorted_evaluation[0:1000]
+            ])
+        )
+        writer.add_text(
+            "Low Prediction/Last batch",
+            "  \n".join([
+                "%s : %s : %s" % (
+                    testing_set.get_classes(idx), y_test[idx, 0].item(), y_evaluation[idx, 0].item())
+                for idx in sorted_evaluation[len(sorted_evaluation) - 1000:len(sorted_evaluation)]
+            ])
+        )
+        writer.flush()
 
     writer.close()
